06f4f4545d03a8179135a89a1fb8226ebd7ddae2
ckhilton <ckirkhilton@gmail.com>
2020-05-25 23:46:39 -0600

    Reorganize HTML-and-CSS-Projects

--- Basic_HTML_and_CSS/honnold_javascript.js
+++
@@ -1,4 +0,0 @@
-function Honnold_Pic_Function() {
-		var pic="free_solo.jpg"
-		document.getElementById("Honnold").src=pic;
-			}	
--- Basic_HTML_and_CSS/words_below_javascript.js
+++
@@ -1,5 +0,0 @@
-function Post_Click_String() {
-			var String = "YEP! THAT'S DANGEROUS!";
-			var result = String.fontcolor("red").fontsize(7).bold().italics();
-			document.getElementById("wow").innerHTML = result;
-		}
