f87785107b6b5faa495f30f5cb7f4f20b1da3c03
ckhilton <ckirkhilton@gmail.com>
2020-06-25 00:41:25 -0600

    Push Website Portfolio

--- Basic_HTML_and_CSS/Portfolio_Website/JS/portfolio_website.js
+++ Basic_HTML_and_CSS/Portfolio_Website/JS/portfolio_website.js
@@ -1,51 +1,33 @@
-// The following function adds a smooth scrolling to all links
+//---------------------------  FORM FUNCTIONS ----------------------------
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
 
-// The following function adds a smooth scrolling to all links
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
 
-$(document).ready(function(){   
-    $("a").on('click', function(event) {
-        
-      // Make sure this.Link has a value before overriding default behavior
-      if (this.Link !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store Link
-        var Link = this.Link;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (2000) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(Link).offset().top
-        }, 2000, function(){
-     
-          // Add Link (#) to URL when done scrolling (default click behavior)
-          window.location.Link = Link;
-        });
-      } // End if
-    });
-  });
-  
-  // These functions open and close the contact form 
-  function openForm() {
-    document.getElementById("myForm").style.display = "block";
+//------------------------- SLIDESHOW FUNCTIONS -------------------------
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-    this.Close();
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-
-
-//      <!-- Custom CSS & Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
-//		<link rel="stylesheet" href="/style.css">
-
-//      <link href="https://fonts.googleapis.com/css2?family=Playfair+Display+SC:wght@900&display=swap" rel="stylesheet">
-
-//		<!-- Custom Fonts -->
-//		<link rel="stylesheet" href="/css/font-awesome/css/font-awesome.min.css">
-//		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-//		<link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-//		<link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-//		<link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-//		<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'></link>
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
